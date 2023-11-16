8d1782723ba85703dd0960111f2380915bbbd872
Pracma16 <oportunilan@gmail.com>
2023-11-16 16:55:24 -0300

    estilos en slider y video corto

--- resources/views/principal.blade.php
+++ resources/views/principal.blade.php
@@ -8,7 +8,7 @@
 <main>
     <div class="baner-video">
       <video class="baner" onloadedmetadata="this .muted=true" autoplay loop style="opacity: 20%;">
-          <source src="/imagenes/Baner-video-link.mp4">
+          <source src="/imagenes/video-baner-corto.mp4">
         </video>
     </div>
     <section class="home" id="home">
@@ -35,19 +35,14 @@
         <h2>CATEGORÍAS DE SERVICIOS</h2>
         <div class="row">
             <!--FILA 1/3: Título -->
-            <div class="col-sm-12">       
-            </div>
+            <div class="col-sm-12"></div>
             <!--FILA 2/3: Carousel con Bootstrap-->
-<<<<<<< HEAD
-            <div id="carouselExample" class="carousel slide"  >
-=======
             <div id="carouselExample" class="carousel slide" style="color:black">
->>>>>>> 86cc27c893298062bec041031975dde58b8bb720
                 <div class="carousel-inner">
                     <!--Slide 1-->
                     <div class="carousel-item active">
                     <div class="cards-wrapper">
-                        <div class="card" style="width: 18rem;">
+                        <div class="card" style="width: 20rem;">
                             <img src="{{asset('imagenes/Comunicacion.jpg')}}" class="card-img-top" alt="...">
                             <div class="card-body">
                               <h5 class="card-title">Card 1</h5>
@@ -55,7 +50,7 @@
                               <a href="#" class="btn btn-primary">Ver comunicación</a>
                             </div>
                           </div>
-                          <div class="card" style="width: 18rem;">
+                          <div class="card" style="width: 20rem;">
                             <img src="{{asset('imagenes/Construccion.jpg')}}" class="card-img-top" alt="...">
                             <div class="card-body">
                               <h5 class="card-title">Card 2</h5>
@@ -63,7 +58,7 @@
                               <a href="#" class="btn btn-primary">Ver construcción</a>
                             </div>
                           </div>
-                          <div class="card" style="width: 18rem;" >
+                          <div class="card" style="width: 20rem;" >
                             <img src="{{asset('imagenes/Mascotas.jpg')}}" class="card-img-top" alt="...">
                             <div class="card-body">
                               <h5 class="card-title">Card 3</h5>
@@ -76,7 +71,7 @@
                   <!--Slide 2-->
                   <div class="carousel-item">
                     <div class="cards-wrapper">
-                        <div class="card" style="width: 18rem;">
+                        <div class="card" style="width: 20rem;">
                             <img src="{{asset('imagenes/Diseño.jpg')}}" class="card-img-top" alt="...">
                             <div class="card-body">
                               <h5 class="card-title">Card 4</h5>
@@ -84,7 +79,7 @@
                               <a href="#" class="btn btn-primary">Ver diseño</a>
                             </div>
                           </div>
-                          <div class="card" style="width: 18rem;">
+                          <div class="card" style="width: 20rem;">
                             <img src="{{asset('imagenes/enfermeria.jpeg')}}" class="card-img-top" alt="...">
                             <div class="card-body">
                               <h5 class="card-title">Card 5</h5>
@@ -92,7 +87,7 @@
                               <a href="#" class="btn btn-primary">Ver enfermeria</a>
                             </div>
                           </div>
-                          <div class="card" style="width: 18rem;">
+                          <div class="card" style="width: 20rem;">
                             <img src="{{asset('imagenes/herramientas-limpieza.jpg')}}" class="card-img-top" alt="...">
                             <div class="card-body">
                               <h5 class="card-title">Card 6</h5>
@@ -105,7 +100,7 @@
                   <!--Slide 3-->
                   <div class="carousel-item">
                     <div class="cards-wrapper">
-                        <div class="card" style="width: 18rem;">
+                        <div class="card" style="width: 20rem;">
                             <img src="{{asset('imagenes/Infancia.jpg')}}" class="card-img-top" alt="...">
                             <div class="card-body">
                               <h5 class="card-title">Card 7</h5>
@@ -113,7 +108,7 @@
                               <a href="#" class="btn btn-primary">Ver infancia</a>
                             </div>
                           </div>
-                          <div class="card" style="width: 18rem;">
+                          <div class="card" style="width: 20rem;">
                             <img src="{{asset('imagenes/Limpieza.jpg')}}" class="card-img-top" alt="...">
                             <div class="card-body">
                               <h5 class="card-title">Card 8</h5>
@@ -121,7 +116,7 @@
                               <a href="#" class="btn btn-primary">Ver limpieza</a>
                             </div>
                           </div>
-                          <div class="card" style="width: 18rem;">
+                          <div class="card" style="width: 20rem;">
                             <img src="{{asset('imagenes/Mascotas.jpg')}}" class="card-img-top" alt="...">
                             <div class="card-body">
                               <h5 class="card-title">Card 9</h5>
@@ -152,35 +147,28 @@
     </div>
 </section>
 
-<!--Section ÚNETE-->
-<section class="about" id="about">
-    <h2>ÚNETE A NOSOTROS</h2>
-    <hr>
-    <p class="lead"><strong>L</strong>orem ipsum dolor, sit amet consectetur adipisicing elit. Officia non nobis ex earum unde enim quo assumenda vel dolore iusto minima, mollitia, voluptatem ut? Enim officiis id nobis pariatur similique ex, architecto iure atque omnis. Alias, amet. Dignissimos est id ipsam inventore, quis fuga ratione, autem nihil iure laudantium adipisci nobis! Inventore, sequi et vel earum culpa ratione nihil labore molestiae, sit esse, cumque consectetur omnis vero asperiores aliquam placeat hic quos quasi amet? Distinctio doloremque culpa ipsam illum beatae alias voluptatum impedit provident, ipsum vero aut omnis? Dicta quos at ipsa odit ullam cumque? Hic veritatis corrupti unde error.<br><strong>S</strong>i Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam assumenda culpa reprehenderit corporis mollitia atque vitae doloremque dignissimos veniam odit. <a id="palabra-contactarme" href="/register-prestador">aquí.</a></p>
-    <hr>
-</section>
-
 <!--Section CONTACTO con form-->
 <section class="contacto" id="contacto">
     <div class="container-contacto">
-        <h2>CONTACTO</h2>
-        <form class="form-contacto" action="https://formspree.io/f/xoqoklrz" method="POST" style="" >
+        
+        <form class="form-principal-contacto" action="https://formspree.io/f/xoqoklrz" method="POST">
+          <h2>CONTACTO</h2>
             <div class="mb-3">
                 <label for="exampleInputName1" class="form-label">Nombre y apellido</label>
-                <input type="text" class="form-control" id="exampleInputName1" placeholder="Escribe aquí tu nombre y apellido" name="nombre">
+                <input type="text"  id="exampleInputName1" placeholder="Escribe aquí tu nombre y apellido" name="nombre">
             </div>
             <div class="mb-3">
                 <label for="exampleInputEmail1" class="form-label">Email</label>
-                <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="name@example.com" name="email">
-                <div id="emailHelp" class="form-text">Nunca enviaremos tu correo a nadie más.</div>
+                <input type="email" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="name@example.com" name="email">
+                <div id="emailHelp" style="color: white; text-align: center;" class="form-text">Nunca enviaremos tu correo a nadie más.</div>
             </div>
             <div class="mb-3">
                 <label for="exampleInputPassword1" class="form-label">Teléfono</label>
-                <input type="number" class="form-control" id="exampleInputPassword1" placeholder="Escribe tu número de contacto" name="telefono">
+                <input type="number"  id="exampleInputPassword1" placeholder="Escribe tu número de contacto" name="telefono">
             </div>
             <div class="mb-3">
                 <label for="exampleFormControlTextarea1" class="form-label">Mensaje</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" name="mensaje" placeholder="Escribe aquí tus comentarios o motivo de consulta"></textarea>
+                <textarea id="exampleFormControlTextarea1"  rows="3" name="mensaje" placeholder="Escribe aquí tus comentarios o motivo de consulta"></textarea>
             </div> 
             <button type="submit" class="btn btn-contacto">Enviar</button>
         </form>   
